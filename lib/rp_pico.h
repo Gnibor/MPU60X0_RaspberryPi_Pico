b6df52d6cf7fada3d3f7b4523ed1961542d892c3
Gnibor <gawin.rg@gmail.com>
2026-03-23 03:01:32 +0000

    Remove: rp_pico.* - log related

--- lib/rp_pico.h
+++ lib/rp_pico.h
@@ -1,27 +1,11 @@
 #ifndef RP_PICO_H
 #define RP_PICO_H
-#include "hardware/i2c.h"
-#include "i2c.h"
-#include "ansi-esc.h"
-#include "pico/stdio_usb.h"
+#include <stdio.h>
+#include "pico/stdlib.h"
 
 #ifndef TESTING_ENABLED
 #define TESTING_ENABLED 1
 #endif
-
-#ifndef INFO_ENABLED
-#define INFO_ENABLED 1
-#endif
-
-#ifndef DEBUG_ENABLED
-#define DEBUG_ENABLED 0
-#endif
-
-#ifndef LOG_NEW_LINE
-#define LOG_NEW_LINE 1
-#endif
-
-#define LOG_PREFIX ANSI_COLOR_FN __func__ ANSI_RESET "():" ANSI_STR(__LINE__)
 
 static inline void pico_stdio_init(void){
 	stdio_init_all();
@@ -69,20 +53,20 @@ static inline pico_time_t pico_get_timestamp(void)
  *
  * @details
  * Supported format tokens:
- * - h = hours (2 digits)
- * - m = minutes (2 digits)
- * - s = seconds (2 digits)
- * - S = milliseconds (3 digits)
+ * - %h = hours (2 digits)
+ * - %m = minutes (2 digits)
+ * - %s = seconds (2 digits)
+ * - %S = milliseconds (3 digits)
  *
  * Any other character is printed unchanged.
  *
  * Example:
  * @code
- * pico_tsprintf("h:m:s");
- * pico_tsprintf("[h:m:s:S] ");
+ * pico_tsprintf("%h:%m:%s");
+ * pico_tsprintf("[%h:%m:%s:%S] ");
  * @endcode
  *
- * @param fmt Format string using h/m/s/S placeholders.
+ * @param fmt Format string using %h/%m/%s/%S placeholders.
  */
 static inline void pico_tsprintf(const char *fmt)
 {
@@ -91,13 +75,19 @@ static inline void pico_tsprintf(const char *fmt)
 	pico_time_t t = pico_get_timestamp();
 
 	while (*fmt) {
-		switch (*fmt) {
-			case 'h': printf("%02u", t.h);  break;
-			case 'm': printf("%02u", t.m);  break;
-			case 's': printf("%02u", t.s);  break;
-			case 'S': printf("%03u", t.ms); break;
-			default:  putchar(*fmt);        break;
-		}
+		if(*fmt == '%' && fmt[1]){
+			fmt++;
+			switch (*fmt) {
+				case 'h': printf("%02u", t.h);  break;
+				case 'm': printf("%02u", t.m);  break;
+				case 's': printf("%02u", t.s);  break;
+				case 'S': printf("%03u", t.ms); break;
+				default:
+					putchar('%');
+					putchar(*fmt);
+					break;
+			}
+		}else putchar(*fmt);
 		fmt++;
 	}
 }
@@ -128,37 +118,5 @@ typedef enum {
 } key_t;
 
 key_t get_key();
-
-// Log Levels
-typedef enum {
-	LOG_INFO,
-	LOG_WARN,
-	LOG_ERROR,
-	LOG_DEBUG
-} log_level_t;
-
-// The core logging function
-void pico_log(log_level_t level, const char *fmt, ...);
-
-// Handy macros for shorter calls
-// #define LOG_W(...) pico_log(LOG_WARN,  __VA_ARGS__)
-//#define LOG_E(...) pico_log(LOG_ERROR, __VA_ARGS__)
-#define LOG_E(fmt, ...) pico_log(LOG_WARN, ANSI_COLOR_FN "%s" ANSI_RESET "():%d: " fmt, __func__, __LINE__, ##__VA_ARGS__)
-#define LOG_W(fmt, ...) pico_log(LOG_WARN, ANSI_COLOR_FN "%s" ANSI_RESET "():%d: " fmt, __func__, __LINE__, ##__VA_ARGS__)
-
-#if INFO_ENABLED
-// #define LOG_I(...) pico_log(LOG_INFO, __VA_ARGS__)
-#define LOG_I(fmt, ...) pico_log(LOG_INFO, ANSI_COLOR_FN "%s" ANSI_RESET "():%d: " fmt, __func__, __LINE__, ##__VA_ARGS__)
-#else
-#define LOG_I(...) ((void)0) // Completely ignored by the compiler
-#endif
-
-// The Magic: If disabled, LOG_D does absolutely nothing
-#if DEBUG_ENABLED
-// #define LOG_D(...) pico_log(LOG_DEBUG, __VA_ARGS__)
-#define LOG_D(fmt, ...) pico_log(LOG_DEBUG, ANSI_COLOR_FN "%s" ANSI_RESET "():%d: " fmt, __func__, __LINE__, ##__VA_ARGS__)
-#else
-#define LOG_D(...) ((void)0) // Completely ignored by the compiler
-#endif
 
 #endif
